5517ab93d2579a85551eab4b0ba0962a4b7686be
devjean <jeancarloleall@gmail.com>
2023-01-23 14:09:45 -0300

    pt-br

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
   const apiKey = "7ef853c3ea72e45701f6a3c983aa4301";
+  // const apiCountryURL = "https://countryflagsapi.com/png/";
   const [waetherData, setWaetherData] = useState([{}]);
   const [city, setCity] = useState("");
 
   const getWeather = (event) => {
     if (event.key === "Enter") {
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}&lang=pt_br`
       )
         .then((response) => response.json())
         .then((data) => {
@@ -37,8 +39,20 @@ function App() {
       ) : (
         <div className="weather-data">
           <p className="city">{waetherData.name}</p>
+          <img
+            className="country"
+            src="https://countryflagsapi.com/png/br"
+            alt=""
+          />
           <p className="temp">{Math.round(waetherData.main.temp)} ºG</p>
-          <p className="weather">{waetherData.weather[0].main}</p>
+          <div className="weather-content">
+            <p className="weather">{waetherData.weather[0].main}</p>
+            <img
+              className="icon-list"
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt="Condições do tempo"
+            />
+          </div>
         </div>
       )}
 
